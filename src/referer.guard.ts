b5c8277215add0dfb6ecadb3ba1ddd47342066cc
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2020-01-08 18:02:03 +0000

    Updated package versions.

--- src/referer.guard.ts
+++ src/referer.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import {Reflector} from '@nestjs/core';
 import { Request } from 'express';
 
 @Injectable()
