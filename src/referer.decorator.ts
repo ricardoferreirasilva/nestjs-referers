cee9787f19a583dbb5bef7964983ec1e35e0f037
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2020-01-10 10:23:39 +0000

    Added some documentation.

--- src/referer.decorator.ts
+++ src/referer.decorator.ts
@@ -1,7 +1,12 @@
-import {UseGuards, SetMetadata,applyDecorators} from '@nestjs/common';
+import {UseGuards, applyDecorators} from '@nestjs/common';
 import { RefererGuard } from './referer.guard';
 import { Referers } from './referers.decorator';
 
+/**
+ * Decorator that guards a controller route against empty or invalid referer headers.
+ * 
+ * @param referers a single referer to validate or a list of referers.
+ */
 export function ValidateReferers(...referers: string[]) {
     return applyDecorators(
         Referers(...referers),
--- src/referer.guard.ts
+++ src/referer.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import {Reflector} from '@nestjs/core';
-import { Request } from 'express';
-
+import {Request} from "express"
 @Injectable()
 export class RefererGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
@@ -9,18 +8,23 @@ export class RefererGuard implements CanActivate {
   canActivate(context: ExecutionContext,): boolean | Promise<boolean> {
     const request : Request = context.switchToHttp().getRequest();
     const requestReferer = request.headers.referer;
+
+    // If the referer exists.
     if(requestReferer){
       const referers = this.reflector.get<string[]>('referers', context.getHandler());
+
+      //For each of specified valid referers.
       for(let referer of referers){
           const refererRegex = this.produceRefererRegex(referer);
           const match = requestReferer.match(refererRegex);
+          //If it matches, request referer is valid.
           if(match) return true;
       }
+      //Referer did not match with any possibilities.
       return false;
     }
-    else{
-      return false;
-    }
+    //If request has no referer, reject.
+    return false;
   }
 
   private produceRefererRegex(referer : string) : RegExp {
